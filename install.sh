028eb5b13e4f0074d6c9a1564154984643be5efc
Brett Wilkins <brett@brett.geek.nz>
2017-01-23 22:32:41 +1100

    Updating vim installer to also do neovim init (assuming Vundle)

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+mkdir -p ~/.vim/bundle
+mkdir -p ~/.config/nvim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.config/nvim/bundle/Vundle.vim
 ln -s $(pwd)/vimrc $HOME/.vimrc
+ln -s $(pwd)/vimrc $HOME/.config/nvim/init.vim
 vim +PluginInstall +qall!
