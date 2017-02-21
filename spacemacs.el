b5406609d85329b1604ddf5417ed8d7440774330
Brett Wilkins <brett@brett.geek.nz>
2017-02-21 22:57:06 +1100

    Adding puppet layer for puppet files

--- spacemacs.el
+++ spacemacs.el
@@ -51,6 +51,7 @@ values."
      ;; version-control
      ruby
      clojure
+     puppet
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
