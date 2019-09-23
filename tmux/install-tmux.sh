e29c7fe311aca69e518773e9e5150335ff8b7135
Brett Wilkins <brett.wilkins@afterpaytouch.com>
2019-09-23 10:46:36 +1000

    Add kitty to dotfiles; update tmux to check for brew first

--- tmux/install-tmux.sh
+++ tmux/install-tmux.sh
@@ -3,7 +3,7 @@
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 
 # OSX-Specific
-if [ "$(uname)" = "Darwin" ]
+if [[ "$(uname)" = "Darwin" && "$(type -P brew)" != "" ]]
 then
   brew install tmux reattach-to-user-namespace
   ln -s $(pwd)/tmux-osx.conf $HOME/.tmux-osx.conf
