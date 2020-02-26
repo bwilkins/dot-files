6a93fdfe5b878c7d7b5f210b2177d79776596ff3
Brett Wilkins <brett@brett.geek.nz>
2020-02-26 12:07:48 +1100

    Adding config changes to emacs to include straight.el and org-roam

--- emacs/emacs.d/load-configs.el
+++ emacs/emacs.d/load-configs.el
@@ -1,20 +1,34 @@
 (require 'package)
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 ;; Have a few repositories here
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-
 (setq package-enable-at-startup nil)
 (package-initialize)
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+(setq
+ package-enable-at-startup nil
+ package-archives
+ '(("melpa-stable" . "http://stable.melpa.org/packages/")
+   ("melpa" . "http://melpa.org/packages/")
+   ("marmalade"   . "http://marmalade-repo.org/packages/")
+   ("org"         . "http://orgmode.org/elpa/")
+   ("gnu"         . "http://elpa.gnu.org/packages/")
+   ("sc"   . "http://joseito.republika.pl/sunrise-commander/")))
 
-(eval-when-compile
-  (require 'use-package))
+
+(straight-use-package 'use-package)
 
 ;; Update local package index
 (unless package-archive-contents
