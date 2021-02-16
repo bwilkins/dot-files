fed00006126012ef3145495f8bbc79507dccb195
Brett Wilkins <brett@brett.geek.nz>
2021-02-17 10:42:02 +1100

    Continue trying to fix projectile/ivy setup

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -3,18 +3,3 @@
 (org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))
 
 (server-start)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-backends
-   '(company-robe company-bbdb company-semantic company-cmake company-capf company-clang company-files
-		  (company-dabbrev-code company-gtags company-etags company-keywords)
-		  company-oddmuse company-dabbrev)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
